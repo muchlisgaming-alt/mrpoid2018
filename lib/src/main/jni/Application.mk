0a558fc02859f02f693404a0977b708eb18786de
muchlisgaming-alt <muchlisgaming@gmail.com>
2026-09-17 05:48:17 +0700

    Update Application.mk

--- lib/src/main/jni/Application.mk
+++ lib/src/main/jni/Application.mk
@@ -1,5 +1,4 @@
 # The ARMv7 is significanly faster due to the use of the hardware FPU
-APP_ABI := armeabi 
-#APP_ABI := armeabi-v7a
-#APP_ABI := armeabi armeabi-v7a
-APP_PLATFORM := android-17
+# DIUBAH: Menggunakan 4 arsitektur untuk dukungan 32-bit dan 64-bit
+APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
+APP_PLATFORM := android-21
